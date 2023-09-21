53bd5e34a05a4a20d694a4cb7d0d3917e983e66d
unknown <agriadhiatma@gmail.com>
2023-09-21 21:27:14 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./assets/css/App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-9xl">Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
